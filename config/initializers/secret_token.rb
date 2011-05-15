ba2a71dea48276650876a9ae30866d7a0b89dfa4
hamhei <hamhei.horiuchi@gmail.com>
2011-05-15 02:39:13 -0700

    this is first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellogithub::Application.config.secret_token = 'b77a0e085223841cf571cf0798d0becbd418e5564dcaa8b0c9f4f320c95fd7e713de0940b7d7f4f9cdafd77e513be7eb8ede051fc5030017f98bbee2478c1c3d'
